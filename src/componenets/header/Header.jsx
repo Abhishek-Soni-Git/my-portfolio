a7b8ef3d2d68c9390f530297ba2e4ebf0a79748f
Abhishek Soni <soniabhishek4567@gmail.com>
2026-05-06 10:41:30 +0530

    file

--- src/componenets/header/Header.jsx
+++ src/componenets/header/Header.jsx
@@ -1,11 +1,14 @@
-//import React from 'react'
 import './header.css'
 import CTA from './CTA'
 import ME from '../../assets/me.jpg'
 
 export default function Header() {
+  // Yahan console.log likhte hain — component ke andar, JSX se pehle
+  console.log(import.meta.env.VITE_APP_NAME)
+  console.log(import.meta.env.VITE_APP_ENV)
+
   return (
-    <section className= "container header_container">
+    <section className="container header_container">
       <h5 className='intro'>Hello i am</h5>
       <h1>Abhishek soni</h1>
       <h5 className="profesion">Devops Engineer</h5>
@@ -14,8 +17,6 @@ export default function Header() {
          <img id='pi' src={ME} alt=""/>
       </div>
       <a href="#about" className='scroll_down'>Scroll Down</a>
-      
     </section>
-  
   )
 }
