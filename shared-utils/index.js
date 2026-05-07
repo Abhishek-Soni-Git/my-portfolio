d9ef2c2ee17058e565421ab41ad939719a21eca8
Abhishek Soni <soniabhishek4567@gmail.com>
2026-05-07 17:10:26 +0530

    new changes

---
+++ shared-utils/index.js
@@ -0,0 +1,11 @@
+export const greet = () => {
+  return "Hello from JFrog private npm package";
+};
+
+export const appName = () => {
+  return "React Portfolio";
+};
+
+export const version = () => {
+  return "1.0.0";
+};
